98a40ec8ad0c057cd10c164e26ce085db1310dec
umarsani1605 <umarsani361@gmail.com>
2026-03-16 03:19:37 +0700

    feat: add RYLS constants

---
+++ app/utils/ryls.ts
@@ -0,0 +1,22 @@
+export interface RylsOption {
+  value: string
+  label: string
+}
+
+export const GENDER_OPTIONS: RylsOption[] = [
+  { value: 'MALE', label: 'Male' },
+  { value: 'FEMALE', label: 'Female' },
+  { value: 'PREFER_NOT_TO_SAY', label: 'Prefer not to say' },
+]
+
+export const DISCOVER_SOURCES: RylsOption[] = [
+  { value: 'RISE_INSTAGRAM', label: 'Rise Social Instagram' },
+  { value: 'OTHER_INSTAGRAM', label: 'Others Instagram account' },
+  { value: 'FRIENDS', label: 'Friends/colleagues' },
+  { value: 'OTHER', label: 'Other' },
+]
+
+export const SCHOLARSHIP_TYPES: RylsOption[] = [
+  { value: 'FULLY_FUNDED', label: 'Fully Funded' },
+  { value: 'SELF_FUNDED', label: 'Self Funded' },
+]
